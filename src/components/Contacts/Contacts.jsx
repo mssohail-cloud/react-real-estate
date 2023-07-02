a8ce64585ecfb04ac9dc7912d8613f07ecfa33ea
Muhammad Sohail <muhammadsohail87@gmail.com>
2023-07-02 05:07:16 +0500

    complete project

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./Contacts.css";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
+
+const Contacts = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left section */}
+        <div className="flexColStart c-left">
+          <span className="orangeText">Our Contacts</span>
+          <span className="primaryText">Easy to Contact Us</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best services for you. We
+            believe a good place to live can make your life better
+          </span>
+
+          <div className="flexColStart contactModes">
+            {/* first row  */}
+            <div className="flexStart row">
+              {/* first mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Call</span>
+                    <span className="secondaryText"> 021 333 444 55</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Call Now</div>
+              </div>
+              {/* second mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat</span>
+                    <span className="secondaryText"> 021 333 444 55</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Chat Now</div>
+              </div>
+            </div>
+
+            {/* second row */}
+
+            <div className="flexStart row">
+              {/* first mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Video Call</span>
+                    <span className="secondaryText"> 021 333 444 55</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Video Call Now</div>
+              </div>
+              {/* second mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Message</span>
+                    <span className="secondaryText"> 021 333 444 55</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Message Now</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* right section */}
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contacts;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <section className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        {/* left section */}
+        <div className="flexColStart f-left">
+          <img src="./logo2.png" alt="" width={120} />
+          <span className="secondaryText">
+            Our vision is to make all people <br /> the best place to live for
+            them
+          </span>
+        </div>
+
+        {/* right section */}
+        <div className="flexColStart f-right">
+          <span className="primaryText">Information</span>
+          <span>123 New York, FL443 , USA</span>
+          <div className="flexCenter f-menu">
+            <span>Property</span>
+            <span>Services</span>
+            <span>Products</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
